14e9081c565647cbbd491f7d599b88f16738fe39
NaturalFish <naturalfish3796@gmail.com>
2015-09-15 20:42:09 +0900

    150915 Day2

---
+++ 예제/js/1-1-1.js
@@ -0,0 +1,8 @@
+﻿function drawScreen() {
+	var theCanvas = document.getElementById("gameCanvas");
+	var Context = theCanvas.getContext('2d'); //렌더링 컨텍스트 : 2d, 3d
+	Context.fillStyle = "#1abc9c";
+	Context.fillRect(0, 0, 1000, 700);
+}
+
+drawScreen();
